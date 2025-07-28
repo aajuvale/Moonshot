8007b6b78cd913c398af05d0b5ea575e7923cc32
aajuvale <ahmed.juvale@gmail.com>
2025-07-28 06:49:08 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ahmed Juvale on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
