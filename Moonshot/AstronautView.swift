894b2542d54ee66792df690a67bd0a094d4c07ae
aajuvale <ahmed.juvale@gmail.com>
2025-07-30 21:16:05 -0700

    feat: implemented and linked astronaut view

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,29 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Ahmed Juvale on 7/30/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
